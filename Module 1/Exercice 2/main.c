c978069e815da910982782a30c97839766ae85e0
gauthy66 <gauthy.dussart66@gmail.com>
2025-11-10 12:15:45 +0100

    Add 'return 0'

--- Module 1/Exercice 2/main.c
+++ Module 1/Exercice 2/main.c
@@ -2,11 +2,12 @@
 
 int main()
 {
-
     int age = 23;
     float taille = 1.73;
     char note = 'A';
 
     printf("L'utilisateur a %d ans et mesure %.2fm.\n", age, taille);
     printf("Il a une note de %c.\n", note);
+
+    return 0;
 }
