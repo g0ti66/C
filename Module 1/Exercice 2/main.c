19b44597118a810cf4ed83e04571db47ed1bd616
gauthy66 <gauthy.dussart66@gmail.com>
2025-11-10 11:42:11 +0100

    Update main.c

--- Module 1/Exercice 2/main.c
+++ Module 1/Exercice 2/main.c
@@ -7,6 +7,6 @@ int main()
     float taille = 1.73;
     char note = 'A';
 
-    printf("L'utilisateur a %d ans et mesure %fm.\n", age, taille);
+    printf("L'utilisateur a %d ans et mesure %.2fm.\n", age, taille);
     printf("Il a une note de %c.\n", note);
 }
