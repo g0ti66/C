5f00277c7c19762461ca8ba0e1df4b4818c66082
gauthy66 <gauthy.dussart66@gmail.com>
2025-12-04 18:33:17 +0100

    Add M3E1

--- Module 3/Exercice 1/main.c
+++ Module 3/Exercice 1/main.c
@@ -3,5 +3,25 @@
 int main()
 {
 
+    int age;
+
+    printf("Entrez votre âge : ");
+    scanf("%d", &age);
+
+    if (age < 18)
+    {
+        printf("Vous êtes mineur !\n");
+    }
+
+    else if (age == 18)
+    {
+        printf("Vous êtes enfin majeur !");
+    }
+
+    else
+    {
+        printf("Vous êtes majeur !");
+    }
+
     return 0;
 }
---
+++ Module 3/README.md
@@ -0,0 +1,4 @@
+# Module 3
+## Exercice 1
+#### Demander à l'utilisateur de rentrer son âge
+#### En fonction de son âge, afficher à l'utilisateur s'il est majeur ou non
