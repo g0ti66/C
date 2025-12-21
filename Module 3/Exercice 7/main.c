22ad586e28f5fe43ad2f68c6e23315f1ca72200f
gauthy66 <gauthy.dussart66@gmail.com>
2025-12-21 19:21:34 +0100

    Update M3E7

--- Module 3/Exercice 7/main.c
+++ Module 3/Exercice 7/main.c
@@ -1,30 +1,19 @@
 #include <stdio.h>
 
 int main() {
+    
+    float nb;
 
-    char number[20];  // Tableau pour stocker le nombre
-    int flag = 0;     // Pour voir si un décimal est trouver (0 = not found, 1 = found)
-    int i;            // Permettra de parcourir le caractère
+    printf("Veuillez rentrer un nb flottant : ");
+    scanf("%f", &nb);
 
-    printf("Enter a number: ");
-    scanf("%s", number);
+    int entier = (int)nb;
 
-    // Regarder chaque caractère pour voir si il est en décimal
-    for(i = 0; number[i] != '\0'; i++) {
-        if(number[i] == '.') {
-            flag = 1;
-            break;
-        }
-    }
-
-    // Affichage du résultat selon le flag
-    if(flag) {
-        printf("Le nombre est un flottant.\n");
+    if (nb != entier) {
+        printf("Ce nombre est un flottant\n");
     } else {
-        printf("Le nombre est un entier.\n");
+        printf("Ce nombre est un entier\n");
     }
 
     return 0;
-
-    // https://labex.io/fr/tutorials/c-determine-integer-or-float-in-c-123267
 }
