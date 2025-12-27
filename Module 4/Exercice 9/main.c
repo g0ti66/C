29f50382c6b35cd9101b519985a201c0fe33ecff
gauthy66 <gauthy.dussart66@gmail.com>
2025-12-27 02:05:34 +0100

    Add M4E9

---
+++ Module 4/Exercice 9/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    int nb;
+
+    printf("Veuiller entrer un nombre entre 3 et 10 : ");
+    scanf("%d", &nb);
+
+    while (nb < 3 || nb > 10)
+    {
+        printf("Erreur, veuiller entrer un nombre entre 3 et 10 : ");
+        scanf("%d", &nb);
+    }
+
+    printf("\n");
+
+    for (int i = 1; i <= nb*2; i+=2)
+    {
+        for (int j = nb*2-1; j > i; j-=2)
+        {
+            printf(" ");
+        }
+        for (int k = 1; k <= i; k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    
+
+
+    return 0;
+}
